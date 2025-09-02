dead9f18cce8626b4e4a699ec356f8ee6c3ff5ab
204g1a0505 <204g1a0505@srit.ac.in>
2025-09-02 12:49:07 +0530

    Initial Commit

--- src/Api.js
+++ src/Api.js
@@ -1,33 +1,21 @@
 
-// import axios from 'axios';
-
-// const API_KEY = 'e4f822c184814c23b1cfb13e457800cf';
-// const BASE_URL = 'https://newsapi.org/v2/top-headlines';
-
-// export const fetchNews = async () => {
-//   try {
-//     const response = await axios.get(BASE_URL, {
-//       params: {
-//         apiKey: API_KEY,
-//         country: 'us',  
-//         pageSize: 20,   
-//       },
-//     });
-//     return response.data.articles;
-//   } catch (error) {
-//     console.error('Error fetching news:', error);
-//     return [];
-//   }
-// }import axios from 'axios';
-
 import axios from 'axios';
+
+const API_KEY = 'e4f822c184814c23b1cfb13e457800cf';
+const BASE_URL = 'https://newsapi.org/v2/top-headlines';
 
 export const fetchNews = async () => {
   try {
-    const response = await axios.get('http://localhost:5000/api/news');
-    return response.data;
+    const response = await axios.get(BASE_URL, {
+      params: {
+        apiKey: API_KEY,
+        country: 'us',  
+        pageSize: 20,   
+      },
+    });
+    return response.data.articles;
   } catch (error) {
     console.error('Error fetching news:', error);
     return [];
   }
-};
+}
